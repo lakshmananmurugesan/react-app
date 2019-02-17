a1e4548aae5bcc3a6ad77d4a399acc475cf87c1b
lakshman <lakshmanmurugesan@gmail.com>
2019-02-17 10:26:19 +0530

    Add SP person

--- src/Person/SimplePerson.js
+++ src/Person/SimplePerson.js
@@ -3,6 +3,7 @@ import React from 'react';
 const SimplePerson = (prop) => {
     return (
         <div>
+            <h1>SP</h1>
             <p>{prop.first_name}</p>
             <p>{prop.last_name}</p>
         </div>
